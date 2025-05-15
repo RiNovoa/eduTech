4dd2f8cd2d197232d63fa956e7a091c425ce411b
Ricardo Novoa Ll. <ri.novoa@duocuc.cl>
2025-05-15 12:23:19 -0400

    Add files via upload

---
+++ eduTech/eduTech/src/test/java/com/example/eduTech/EduTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eduTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
